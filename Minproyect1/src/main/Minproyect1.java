e10435a2e5a1383bc9dbcecefefc8b5b9649b425
isabb28 <Isabellabb022@gmail.com>
2023-10-03 00:33:58 -0500

    Inicio funcional

--- Minproyect1/src/main/Minproyect1.java
+++ Minproyect1/src/main/Minproyect1.java
@@ -1,27 +1,22 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package main;
+
+import vista.Ventana;
 
 /**
  *
  * @author sandr
  */
 public class Minproyect1 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-   // create an object of Main
-    Inicio obj = new Inicio();
-
-    // calling method with no parameter
-    obj.display1();
     
+    public static void main(String [] args){
+        
+        Ventana obj = new Ventana();
+        obj.setVisible(true);
+        
     
-    }
-    
+}
 }
